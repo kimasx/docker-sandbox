1da270bad2934bf8ecc57c9acced217ff7e14547
Sun Kim <sunkim@Suns-Air.local>
2020-05-23 20:27:33 -0400

    change feature branch

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          CHANGED
+          CHANGED ON FEATURE NOW, NOT MASTER
         </p>
         <a
           className="App-link"
